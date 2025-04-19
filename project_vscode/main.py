297a3be14b3da00e9a60320cf56e377bfa8e3d2c
Loading7768 <yuchensean@gmail.com>
2025-04-19 10:53:24 +0800

    修改 email 中的 body 問題

--- project_vscode/main.py
+++ project_vscode/main.py
@@ -177,7 +177,7 @@ async def main():
     client = Client(language='en-US')
     client.load_cookies('cookies.json')  # 這裡 **不用 await**，因為是同步函式
 
-    body = ""
+    body = ""  # 用來記錄每一輪的 analysis 來傳 email
 
     # 設定目前是否達到此帳號抓文的上限
     TooManyRequests_bool = False
@@ -364,6 +364,7 @@ async def main():
 
 
         # 在有抓到資料的前提下 把資料存入 analysis.txt 裡
+        analysis_temp = ""
         if founded_count > 0:
             analysisFile = 'analysis.txt'
             analysisTempFile = 'analysis_temp.txt'
@@ -371,7 +372,11 @@ async def main():
                 analysis_temp = file.read()
             with open(analysisFile, 'a', encoding='utf-8-sig') as txtfile:
                 txtfile.write(analysis_temp)
-        body = body + f"{analysis_temp}\n\n"
+        
+        if analysis_temp != "":
+            body = body + f"{analysis_temp}\n\n"
+        else:  # 如果一整天都沒有抓到推文
+            body = body + f"{QUERY}\n本日找不到推文\n\n"
 
         if TooManyRequests_bool:
             break
--- project_vscode/resetCount.py
+++ project_vscode/resetCount.py
@@ -1,16 +1,19 @@
 # 如果 tweet_count 不是從 1 開始的話可以使用
 import json
 
-filename = './data/DOGE/2025/DOGE_20250310.json'  # 修改成指定檔案
+JSON_DICT_NAME = "realDonaldTrump"
+
+filename = './data/realDonaldTrump.json'  # 修改成指定檔案
 with open(filename, 'r', encoding='utf-8-sig') as file:
     data_json = json.load(file)
 
 count = 0
+last_text = data_json[JSON_DICT_NAME][-1]['text']
 while True:
-    number = data_json['dogecoin'][count]['tweet_count']
-    data_json['dogecoin'][count]['tweet_count'] = count + 1
+    number = data_json[JSON_DICT_NAME][count]['tweet_count']
+    data_json[JSON_DICT_NAME][count]['tweet_count'] = count + 1
     
-    if number == 4666:  # 修改成最後一個 tweet_count
+    if data_json[JSON_DICT_NAME][count]['text'] == last_text:
         break
 
     count += 1
