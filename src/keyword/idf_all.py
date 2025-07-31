19ed4a39ec304557e37d0f42dd0bdc1a4f36a506
DrifterAldvvyn <maestroKahlazar@proton.me>
2025-07-31 16:08:04 +0800

    Update idf_all.py
    Added DAILY_MODE

--- src/keyword/idf_all.py
+++ src/keyword/idf_all.py
@@ -8,6 +8,7 @@ redering the value negligible in the same tweet.
 
 from glob import glob
 from datetime import datetime
+from dateutil.rrule import rrule, DAILY
 import bisect
 import json
 import re
@@ -17,6 +18,9 @@ from nltk.corpus import stopwords
 import math
 import os
 
+'''
+Importing parameters from project_root/config.py
+'''
 from pathlib import Path
 import sys
 parent_dir = Path(__file__).resolve().parent.parent
@@ -24,6 +28,11 @@ sys.path.append(str(parent_dir))
 from config import JSON_DICT_NAME
 
 # ----------parameters----------
+'''
+Top nth global keywords recorded in the result.
+'''
+GLOBAL_KEYWORDS_AMOUNT = 100 
+
 '''
 You can modify SET_RANGE to set whether date range is applied,
 'False' will use every file in the folder,
@@ -33,27 +42,25 @@ Set the date using datetime(Y, D, M), no need to fill in extra 0s.
 '''
 SET_RANGE = True
 START_DATE = datetime(2025, 1, 1)
-END_DATE = datetime(2025, 1, 31)
+END_DATE = datetime(2025, 1, 1)
+
+'''
+DAILY_MODE rums IDF day by day with the specified time frame.
+*NOTE* You do need to put SET_RANGE to True. 
+
+Set the date using datetime(Y, D, M), no need to fill in extra 0s.
+'''
+DAILY_MODE = True
+DAILY_START_DATE = datetime(2025, 1, 1)
+DAILY_END_DATE = datetime(2025, 6, 30)
 # ----------parameters----------
-
-# sanitize tweets
-def clean_tweets(text):
-    # deprecated since removing elements prior might interfere with tokenization
-    # text = re.sub(r'http\S+', '', text)   # remove URLs
-    # text = re.sub(r'@\w+', '', text)      # remove mentions
-    # text = re.sub(r'#\w+', '', text)      # remove hashtags
-    # text = text.replace(COIN_SHORT_NAME.lower(), "")
-    
-    text = text.lower()
-    text = text.strip()
-
-    return text
 
 # load tweets
 def load_tweets():
     tweets = []
 
-    json_files = glob(f'../data/filtered_tweets/normal_tweets/*/*/*')
+    # glob every file under ../data/filtered_tweets/normal_tweets/YYYY/mm
+    json_files = sorted(glob(f'../data/filtered_tweets/normal_tweets/*/*/*'))
     if not json_files:
         print(f'No files found in the directory.')
         return []
@@ -64,11 +71,11 @@ def load_tweets():
         for json_file in json_files:
             match_date = re.search(r'\d{8}', json_file)
             file_dates.append(datetime.strptime(match_date.group(), '%Y%m%d'))
-        
+
         left = bisect.bisect_left(file_dates, START_DATE)
         right = bisect.bisect_right(file_dates, END_DATE)
         if left == right:   # no file found with in set range
-            print(f'No files found between {datetime.strftime(START_DATE, '%Y%m%d')} and {datetime.strftime(END_DATE, '%Y%m%d')}')
+            print('No files found within the time frame.')
             return []
         json_files = json_files[left:right]
 
@@ -92,7 +99,7 @@ def tokenize_tweets(tweets):
     tokenized_tweets = []
 
     for tweet in tweets:
-        tweet_text = clean_tweets(tweet.get('text'))
+        tweet_text = tweet.get('text')
         tokens = tokenizer.tokenize(tweet_text)
         unique_tokens = set(token for token in tokens if token not in STOPWORDS and token.isalnum())
         tokenized_tweets.append(unique_tokens)
@@ -125,18 +132,22 @@ def find_global_keyword(tokenized_tweets, idf, top_n=3):
 
     for tweet_tokens in tokenized_tweets:
         token_idfs = [(token, idf.get(token, 0)) for token in tweet_tokens]
-        top_keywrods = sorted(token_idfs, key=lambda x: x[1], reverse=True)[:top_n]
+        top_keywords = sorted(token_idfs, key=lambda x: x[1], reverse=True)[:top_n]
         
-        # print(top_keywrods) if len(top_keywrods) < 3 else None # for debugging
-        for k in top_keywrods:
+        # print(top_keywords) if len(top_keywords) < 3 else None # for debugging
+        for k in top_keywords:
             if k[1] > 3:
                 global_keywords[k[0]] += 1
 
     return sorted(global_keywords.items(), key=lambda x: x[1], reverse=True)
 
 def main():
+    # msg to signal that special mode is ON
     if SET_RANGE:
-        print(f'SET_RANGE is on. Only tweets from {datetime.strftime(START_DATE, '%Y%m%d')} to {datetime.strftime(END_DATE, '%Y%m%d')} will be processed.')
+        if START_DATE == END_DATE:
+            print(f'SET_RANGE is ON. Only tweets on {datetime.strftime(START_DATE, '%Y%m%d')} will be processed.')
+        else:
+            print(f'SET_RANGE is ON. Only tweets from {datetime.strftime(START_DATE, '%Y%m%d')} to {datetime.strftime(END_DATE, '%Y%m%d')} will be processed.')
 
     # load tweets
     tweets = load_tweets()
@@ -148,7 +159,7 @@ def main():
     tokenized_tweets = tokenize_tweets(tweets)
     idf = compute_idf(len(tweets), tokenized_tweets)
     global_keywords = find_global_keyword(tokenized_tweets, idf)
-    top_keywords = [{'keyword': keyword, 'votes': votes, 'idf': idf.get(keyword, 0)} for keyword, votes in global_keywords[:30]]
+    top_keywords = [{'keyword': keyword, 'votes': votes, 'idf': idf.get(keyword, 0)} for keyword, votes in global_keywords[:GLOBAL_KEYWORDS_AMOUNT]]
     print('-' * 80)
 
 
@@ -163,7 +174,10 @@ def main():
     print('Saving results to json file...')
     output_path= f'../data/keyword'
     if SET_RANGE:
-        output_path += f'/{datetime.strftime(START_DATE, '%Y%m%d')}_{datetime.strftime(END_DATE, '%Y%m%d')}'
+        if START_DATE == END_DATE:
+            output_path += f'/{datetime.strftime(START_DATE, '%Y%m%d')}'
+        else:
+            output_path += f'/{datetime.strftime(START_DATE, '%Y%m%d')}_{datetime.strftime(END_DATE, '%Y%m%d')}'
     else:
         output_path += '/all'
     os.makedirs(output_path, exist_ok=True)
@@ -173,20 +187,28 @@ def main():
         json.dump(top_keywords, file, indent=4)
 
     # isolate tweets by keywords
-    for idx, keyword in enumerate(top_keywords):
-        tweets_by_keyword = []
-        tweet_counter = 1
-        for tweet, tokens in zip(tweets, tokenized_tweets):
-            if keyword['keyword'] in tokens:
-                tweet['tweet_count'] = tweet_counter
-                tweet_counter += 1
-                tweets_by_keyword.append(tweet)
-        
-        with open(f'{output_path}/{idx+1:02d}_{keyword['keyword']}.json', 'w', encoding='utf-8') as file:
-            json.dump(tweets_by_keyword, file, indent=4, ensure_ascii=False)
+    if not DAILY_MODE:
+        for idx, keyword in enumerate(top_keywords):
+            tweets_by_keyword = []
+            tweet_counter = 1
+            for tweet, tokens in zip(tweets, tokenized_tweets):
+                if keyword['keyword'] in tokens:
+                    tweet['tweet_count'] = tweet_counter
+                    tweet_counter += 1
+                    tweets_by_keyword.append(tweet)
+            
+            with open(f'{output_path}/{idx+1:02d}_{keyword['keyword']}.json', 'w', encoding='utf-8') as file:
+                json.dump(tweets_by_keyword, file, indent=4, ensure_ascii=False)
     print()
     print('Done.')
     print('-' * 80)
 
 if __name__ == '__main__':
-    main()      
+    if DAILY_MODE:
+        print(f'DAILY_MODE is ON. The script will iterate from {datetime.strftime(DAILY_START_DATE, '%Y%m%d')} to {datetime.strftime(DAILY_END_DATE, '%Y%m%d')}.')
+        SET_RANGE = True # just incase someone didn't read the NOTE.
+        for date in rrule(DAILY, dtstart=DAILY_START_DATE, until=DAILY_END_DATE):
+            START_DATE = END_DATE = date
+            main()
+    else:
+        main()      
