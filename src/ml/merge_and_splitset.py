8e0969bf226aa0c84bc9f1c6b6ae6add81bc77f8
Loading7768 <yuchensean@gmail.com>
2025-08-29 23:29:20 +0800

    更新訓練資料

--- src/ml/merge_and_splitset.py
+++ src/ml/merge_and_splitset.py
@@ -4,13 +4,14 @@ import numpy as np
 import random
 from scipy import sparse
 import os
+import pickle
 
 
 
 '''可修改參數'''
 INPUT_PATH = "../data/ml/dataset"
 
-MIN_COUNT = 2  # 設定刪掉出現次數 <= MIN_COUNT 的關鍵詞 (column)
+MIN_COUNT = 10  # 設定刪掉出現次數 <= MIN_COUNT 的關鍵詞 (column)
 
 TOLERANCE = 100  # 資料集中誤差推文數值 (100 => +-100)
 
@@ -101,11 +102,34 @@ def expand_by_tweet(df):
 
 
 
+# --- 列印漲跌比例 ---
+def print_label_distribution(Y_train, Y_val, Y_test, COIN_SHORT_NAME, split_val=False):
+    def get_stats(y):
+        up = np.sum(y >= 0)
+        down = np.sum(y < 0)
+        total = len(y)
+        return up, down, total, up / total, down / total
+
+    train_up, train_down, train_total, train_up_ratio, train_down_ratio = get_stats(Y_train)
+    if split_val:
+        val_up, val_down, val_total, val_up_ratio, val_down_ratio = get_stats(Y_val)
+    test_up, test_down, test_total, test_up_ratio, test_down_ratio = get_stats(Y_test)
+
+    print(f"{COIN_SHORT_NAME} 標籤分佈：")
+    print(f"  Train: 總數 {train_total}, 漲 {train_up} ({train_up_ratio:.2%}), 跌 {train_down} ({train_down_ratio:.2%})")
+    if split_val:
+        print(f"  Val  : 總數 {val_total}, 漲 {val_up} ({val_up_ratio:.2%}), 跌 {val_down} ({val_down_ratio:.2%})")
+    print(f"  Test : 總數 {test_total}, 漲 {test_up} ({test_up_ratio:.2%}), 跌 {test_down} ({test_down_ratio:.2%})\n")
+
+
+
 # --- 用日期為單位把資料切成 8:1:1 (train : validation : test) ---
 def splitset_dates(COIN_SHORT_NAME):
 
-    # --- 讀取每條推文的日期 npy ---
-    dates = np.load(f"{INPUT_PATH}/keyword/{COIN_SHORT_NAME}_dates.npy")  # array([b'2013-12-15', ...])
+    # --- 讀取每條推文的 ID .pkl ---
+    with open(f"{INPUT_PATH}/keyword/{COIN_SHORT_NAME}_ids.pkl", "rb") as f:   # rb = read binary
+        ids = pickle.load(f)  # array[('coin', 'date', 'no.'), (str, '%Y-%m-%d', int)]
+    dates = np.array([row[1] for row in ids])  # 只把 'date' 取出來，並轉成 np.array
     
     # 如果是 bytes，要轉成 str
     if isinstance(dates[0], bytes):
@@ -177,29 +201,21 @@ def splitset_XY(COIN_SHORT_NAME, split_val=False):
 
     # 讀取稀疏矩陣
     X = sparse.load_npz(f"{INPUT_PATH}/keyword/{COIN_SHORT_NAME}_X_sparse.npz")  # 二維陣列：colunm(關鍵詞) row(某天某推文) (但這裡是稀疏矩陣的格式)
-    dates = np.load(f"{INPUT_PATH}/keyword/{COIN_SHORT_NAME}_dates.npy")  # 一維陣列：存放與 X row 對應的日期
+    
+    # 一維陣列：存放與 X row 對應的 ID
+    with open(f"{INPUT_PATH}/keyword/{COIN_SHORT_NAME}_ids.pkl", "rb") as f:   # rb = read binary
+        ids = pickle.load(f)  # array[('coin', 'date', 'no.'), (str, '%Y-%m-%d', int)]
+    dates = np.array([row[1] for row in ids])  # 只把 'date' 取出來，並轉成 np.array
 
     # 讀取 price_diff.npy
     Y = np.load(f"{INPUT_PATH}/coin_price/{COIN_SHORT_NAME}_price_diff.npy")  # shape = (總推文數,)
-
-    # 先把要刪掉的推文刪掉 利用其他 .py 生成出的 index
-    with open(f"{INPUT_PATH}/keyword/{COIN_SHORT_NAME}_removed_tweets.json", "r", encoding="utf-8") as f:
-        removed_tweets = json.load(f)
-    removed_index = [item["index"] for item in removed_tweets]  # 取出 index 列表
 
     # 輸出長度 確保一致性
     print(f"{COIN_SHORT_NAME}：")
     print("X.shape[0] =", X.shape[0])
     print("dates.shape[0] =", len(dates))
-    print("Y.shape[0] =", len(Y))
-    print()
+    print("Y.shape[0] =", len(Y), "(原始長度 未刪剪)\n")
 
-
-    # 把被刪掉的推文刪掉 (刪 Y) (其中 X_sparse.npz, dates.npy 已經是刪好的資料了)
-    if len(removed_index) > 0:
-        keep_mask = np.ones(len(Y), dtype=bool)
-        keep_mask[removed_index] = False
-        Y = Y[keep_mask]
 
 
     # 把 date 轉成 datetime 格式，方便比對
@@ -242,12 +258,134 @@ def splitset_XY(COIN_SHORT_NAME, split_val=False):
         dates_test = np.concatenate([dates_val, dates_test])
         X_val, Y_val, dates_val = None, None, None  # 不返回 validation
 
+    # 列印各個幣種 split 後 每個資料集的漲跌比例
+    print_label_distribution(Y_train, Y_val, Y_test, COIN_SHORT_NAME)
+
+    return X_train, X_val, X_test, Y_train, Y_val, Y_test, dates_train, dates_val, dates_test
+
+
+
+# --- 將不必要的關鍵詞與推文刪除 ---
+def filter_XY(X_train, X_val, X_test, Y_train, Y_val, Y_test, dates_train, dates_val, dates_test, all_vocab):
+    '''重複 功能 1, 功能 2, 功能 3 直到沒有東西可以刪為止'''
+
+    # 定義 功能 1
+    def function_1(X, Y, dates):
+        row_sums = np.array(X.sum(axis=1)).ravel()
+        valid_rows = np.where(row_sums > 0)[0]
+        invalid_rows = X.shape[0] - len(valid_rows)
+
+        X = X[valid_rows, :]
+        Y = Y[valid_rows]
+        dates = dates[valid_rows]
+        
+        return X, Y, dates, invalid_rows, len(valid_rows)
+            
+
+    invalid_rows = -1
+    delete_min_count = -1
+    delete_only_test = -1
+
+    total_delete_rows = 0
+    total_delete_columns = 0
+
+    # 若還有功能是可以刪資料的，就再繼續跑
+    while invalid_rows != 0 or delete_min_count != 0 or delete_only_test != 0:
+
+        # --- 功能 1: 刪掉沒有任何關鍵詞的推文 (刪 row) ---
+        # train
+        X_train, Y_train, dates_train, train_invalid_rows, train_valid_rows = function_1(X_train, Y_train, dates_train)
+        
+        # test
+        X_test, Y_test, dates_test, test_invalid_rows, test_valid_rows = function_1(X_test, Y_test, dates_test)
+
+        # val
+        if X_val is not None:
+            X_val, Y_val, dates_val, val_invalid_rows, val_valid_rows = function_1(X_val, Y_val, dates_val)
+
+        # 計算保留、刪掉的筆數
+        valid_rows = train_valid_rows + test_valid_rows
+        invalid_rows = train_invalid_rows + test_invalid_rows
+        if X_val is not None:
+            valid_rows += val_valid_rows
+            invalid_rows += val_invalid_rows
+            
+        # 只看 Train 的數量
+        total_delete_rows += train_invalid_rows
+        print("功能 1: 刪掉沒有任何關鍵詞的推文 (row):")
+        print(f"\tTrain 保留 row 數量: {train_valid_rows}")
+        print(f"\tTrain 刪掉 row 數量: {train_invalid_rows}\n")
+
+
+        # --- 功能 2: 刪掉在 train 中出現次數 <= min_count 的關鍵詞 (刪 column) ---
+        col_sums = np.array(X_train.sum(axis=0)).ravel()
+        valid_cols = np.where(col_sums > MIN_COUNT)[0]
+
+        # 每個關鍵詞的出現次數統計
+        keyword_counts = {all_vocab[i]: int(col_sums[i]) for i in range(len(all_vocab))}
+        stats_output_path = os.path.join(INPUT_PATH, "keyword", "keyword_counts.json")
+        with open(stats_output_path, "w", encoding="utf-8") as f:
+            json.dump(keyword_counts, f, ensure_ascii=False, indent=4)
+
+        # 開始過濾
+        X_train = X_train[:, valid_cols]
+        X_test  = X_test[:, valid_cols]
+        if X_val is not None:
+            X_val = X_val[:, valid_cols]
+
+        filtered_vocab = [all_vocab[i] for i in valid_cols]
+        delete_min_count = len(all_vocab) - len(filtered_vocab)
+        total_delete_columns += delete_min_count
+        print("功能 2: 刪掉出現次數 <= min_count 的關鍵詞 (column):")
+        print(f"\t原始 column 數量: {len(all_vocab)}")
+        print(f"\t保留 column 數量: {len(filtered_vocab)}")
+        print(f"\t刪掉 column 數量: {delete_min_count}\n")
+
+        with open(os.path.join(f"{INPUT_PATH}/keyword", f"filtered_keywords.json"), "w", encoding="utf-8") as f:
+            json.dump(filtered_vocab, f, ensure_ascii=False, indent=4)  
+
+
+        # --- 功能 3: 只保留 train 出現過的關鍵詞 (刪 column) --- 
+        '''
+        X_train.nonzero() 會回傳一個 tuple (row_idx, col_idx)：
+        row_idx → 非零元素所在的 row（推文 index）。
+        col_idx → 非零元素所在的 column（關鍵詞 index）。
+
+        X_train.nonzero()[1] 取的是所有非零值的 column index。
+        → 這就是「有哪些關鍵詞至少在 train 裡出現過一次」。
+
+        np.unique(...) 把它去重，得到一個 只出現在 train 的 column 清單。
+        '''
+
+        orig_cols = X_train.shape[1]
+        keep_cols = np.unique(X_train.nonzero()[1])  # train 出現過的 column index
+        new_cols = len(keep_cols)
+
+        # column 過濾
+        X_train = X_train[:, keep_cols]  # [:, keep_cols] 表示「保留所有 row，但只取出 keep_cols 這些 column」。
+        X_test  = X_test[:, keep_cols]
+        if X_val is not None:
+            X_val = X_val[:, keep_cols]
+        if all_vocab is not None:
+            all_vocab = [all_vocab[i] for i in keep_cols]
+
+        delete_only_test = orig_cols - new_cols
+        total_delete_columns += delete_only_test
+        print("功能 3: 只保留 train 出現過的關鍵詞 (column):")
+        print(f"\t原始 column 數量: {orig_cols}")
+        print(f"\t保留 column 數量: {new_cols}")
+        print(f"\t刪掉 column 數量: {delete_only_test}\n")
+
+    print(f"總共刪除 {total_delete_rows} 個推文 (row), {total_delete_columns} 個關鍵詞 (column)\n")
+    print(f"已輸出所有關鍵詞出現次數統計到 {stats_output_path}")
+    print(f"已輸出所有被過濾的關鍵詞到 {INPUT_PATH}/keyword\n")
+
     return X_train, X_val, X_test, Y_train, Y_val, Y_test, dates_train, dates_val, dates_test
 
 
 
 # --- 打亂順序 (shuffle) ---
-def shuffle_xy(X, Y, dates, seed=42):
+def shuffle_XY(X, Y, dates, seed=42):
     """
     Shuffle X and Y in unison.
     X: np.ndarray 或 scipy.sparse 矩陣
@@ -273,6 +411,29 @@ def shuffle_xy(X, Y, dates, seed=42):
 
 
 
+# --- 計算每個日期總共的數量 (用以確認是否有切正確) ---
+def count_per_day(dates, dataset_name):
+    """
+    dates: array-like, 每條推文的日期 (str 或 np.datetime64)
+    dataset_name: 用於打印
+    """
+    # 如果是 bytes，先轉成 str
+    if isinstance(dates[0], bytes):
+        dates = dates.astype(str)
+
+    # 轉成 datetime
+    dates_dt = pd.to_datetime(dates)
+
+    # 計算每天出現次數
+    date_counts = dates_dt.value_counts().sort_index()  # 按日期排序
+    df_counts = pd.DataFrame({"date": date_counts.index, "tweet_count": date_counts.values})
+
+    df_counts.to_csv(f"{INPUT_PATH}/dates_{dataset_name}_counts.csv", index=False)
+
+    return df_counts
+
+
+
 # --- 將三種幣種的 X, Y 合併成完整的模型輸入值 (輸出 .npy 檔) ---
 def merge(DOGE_X_train, DOGE_X_val, DOGE_X_test, DOGE_Y_train, DOGE_Y_val, DOGE_Y_test,
           PEPE_X_train, PEPE_X_val, PEPE_X_test, PEPE_Y_train, PEPE_Y_val, PEPE_Y_test,
@@ -280,7 +441,7 @@ def merge(DOGE_X_train, DOGE_X_val, DOGE_X_test, DOGE_Y_train, DOGE_Y_val, DOGE_
           DOGE_dates_train, DOGE_dates_val, DOGE_dates_test,
           PEPE_dates_train, PEPE_dates_val, PEPE_dates_test,
           TRUMP_dates_train, TRUMP_dates_val, TRUMP_dates_test,
-          min_count, all_vocab=None):
+          all_vocab):
 
     # 合併 X（稀疏矩陣用 sparse.vstack）
     X_train_list = [DOGE_X_train, PEPE_X_train, TRUMP_X_train]
@@ -302,60 +463,16 @@ def merge(DOGE_X_train, DOGE_X_val, DOGE_X_test, DOGE_Y_train, DOGE_Y_val, DOGE_
     dates_val   = np.concatenate([DOGE_dates_val, PEPE_dates_val, TRUMP_dates_val]) if DOGE_dates_val is not None else None
 
 
-    '''
-    X_train.nonzero() 會回傳一個 tuple (row_idx, col_idx)：
-    row_idx → 非零元素所在的 row（推文 index）。
-    col_idx → 非零元素所在的 column（關鍵詞 index）。
-
-    X_train.nonzero()[1] 取的是所有非零值的 column index。
-    → 這就是「有哪些關鍵詞至少在 train 裡出現過一次」。
-
-    np.unique(...) 把它去重，得到一個 只出現在 train 的 column 清單。
-    '''
-
-    # 只保留 train 出現過的關鍵詞 (column)
-    orig_cols = X_train.shape[1]
-    keep_cols = np.unique(X_train.nonzero()[1])  # train 出現過的 column index
-    new_cols = len(keep_cols)
-
-    # column 過濾
-    X_train = X_train[:, keep_cols]  # [:, keep_cols] 表示「保留所有 row，但只取出 keep_cols 這些 column」。
-    X_test  = X_test[:, keep_cols]
-    if X_val is not None:
-        X_val = X_val[:, keep_cols]
-    if all_vocab is not None:
-        all_vocab = [all_vocab[i] for i in keep_cols]
-
-    print("只保留 train 出現過的關鍵詞 (column):")
-    print(f"\t原始 column 數量: {orig_cols}")
-    print(f"\t保留 column 數量: {new_cols}")
-    print(f"\t刪掉 column 數量: {orig_cols - new_cols}\n")
-
-    # 刪掉出現次數 <= min_count 的 column
-    col_sums = np.array(X_train.sum(axis=0)).ravel()
-    valid_cols = np.where(col_sums > min_count)[0]
-
-    X_train = X_train[:, valid_cols]
-    X_test  = X_test[:, valid_cols]
-    if X_val is not None:
-        X_val = X_val[:, valid_cols]
-    if all_vocab is not None:
-        filtered_vocab = [all_vocab[i] for i in valid_cols]
-        print("刪掉出現次數 <= min_count 的關鍵詞 (column):")
-        print(f"\t原始 column 數量: {len(all_vocab)}")
-        print(f"\t保留 column 數量: {len(filtered_vocab)}")
-        print(f"\t刪掉 column 數量: {len(all_vocab) - len(filtered_vocab)}\n")
-
-    with open(os.path.join(f"{INPUT_PATH}/keyword", f"filtered_keywords.json"), "w", encoding="utf-8") as f:
-        json.dump(filtered_vocab, f, ensure_ascii=False, indent=4)  
-
+    # 將不必要的關鍵詞與推文刪除
+    X_train, X_val, X_test, Y_train, Y_val, Y_test, dates_train, dates_val, dates_test = filter_XY(X_train, X_val, X_test, Y_train, Y_val, Y_test, dates_train, dates_val, dates_test, all_vocab)
+    
 
 
     # 打亂順序
-    X_train, Y_train, dates_train = shuffle_xy(X_train, Y_train, dates_train)
-    X_test,  Y_test,  dates_test  = shuffle_xy(X_test, Y_test, dates_test)
+    X_train, Y_train, dates_train = shuffle_XY(X_train, Y_train, dates_train)
+    X_test,  Y_test,  dates_test  = shuffle_XY(X_test, Y_test, dates_test)
     if X_val is not None:
-        X_val, Y_val, dates_val = shuffle_xy(X_val, Y_val, dates_val)
+        X_val, Y_val, dates_val = shuffle_XY(X_val, Y_val, dates_val)
 
     # 儲存
     sparse.save_npz(f"{INPUT_PATH}/X_train.npz", X_train)
@@ -368,10 +485,13 @@ def merge(DOGE_X_train, DOGE_X_val, DOGE_X_test, DOGE_Y_train, DOGE_Y_val, DOGE_
         np.save(f"{INPUT_PATH}/Y_val.npy", Y_val)
 
     if dates_train is not None:
-        np.save(f"{INPUT_PATH}/dates_train.npy", dates_train)
-        np.save(f"{INPUT_PATH}/dates_test.npy", dates_test)
+        np.savez_compressed(f"{INPUT_PATH}/dates_train.npz", arr=dates_train)
+        np.savez_compressed(f"{INPUT_PATH}/dates_test.npz", arr=dates_test)
+        # np.save(f"{INPUT_PATH}/dates_train.npy", dates_train)
+        # np.save(f"{INPUT_PATH}/dates_test.npy", dates_test)
         if dates_val is not None:
-            np.save(f"{INPUT_PATH}/dates_val.npy", dates_val)
+            np.savez_compressed(f"{INPUT_PATH}/dates_val.npz", arr=dates_val)
+            # np.save(f"{INPUT_PATH}/dates_val.npy", dates_val)
 
     # 檢查資料集維度
     assert X_train.shape[0] == len(Y_train) == len(dates_train), "Train 維度不一致!"
@@ -380,7 +500,20 @@ def merge(DOGE_X_train, DOGE_X_val, DOGE_X_test, DOGE_Y_train, DOGE_Y_val, DOGE_
         assert X_val.shape[0] == len(Y_val) == len(dates_val), "Val 維度不一致!"
 
 
-    print("Merge 完成，資料已輸出到 ../data/ml/dataset")
+    print("Merge 完成，資料已輸出到 ../data/ml/dataset\n")
+
+    # 列印 merge, filter 後 每個資料集的漲跌比例
+    print_label_distribution(Y_train, Y_val, Y_test, "ALL")
+
+    # 計算每個資料集中每天的推文總數
+    count_per_day(dates_train, "train")
+    count_per_day(dates_test, "test")
+    if dates_val is not None:
+        count_per_day(dates_val, "val")
+
+    print("已將不同資料集每天的推文總數輸出為 csv 到 ../data/ml/dataset\n")
+
+
 
 
 
@@ -408,6 +541,7 @@ def main():
     print_split_number(TRUMP_dates_train_expanded, TRUMP_dates_val_expanded, TRUMP_dates_test_expanded, "TRUMP")
     TRUMP_X_train, TRUMP_X_val, TRUMP_X_test, TRUMP_Y_train, TRUMP_Y_val, TRUMP_Y_test, TRUMP_dates_train, TRUMP_dates_val, TRUMP_dates_test = splitset_XY("TRUMP")
 
+    # 讀取所有關鍵詞的名字
     json_path = os.path.join("../data/keyword/machine_learning", "all_keywords.json")
     with open(json_path, "r", encoding="utf-8") as f:
         vocab = json.load(f)
@@ -421,7 +555,7 @@ def main():
           DOGE_dates_train, DOGE_dates_val, DOGE_dates_test,
           PEPE_dates_train, PEPE_dates_val, PEPE_dates_test,
           TRUMP_dates_train, TRUMP_dates_val, TRUMP_dates_test,
-          MIN_COUNT, all_vocab)  # 刪掉出現次數 <= min_count 的 column
+          all_vocab)
     
 
 if __name__ == "__main__":
